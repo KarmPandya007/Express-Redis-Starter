e905ca0ca7b54d015a6d61e27c0c9a0391f95c0c
KarmPandya007 <karmpandya007@gmail.com>
2026-01-21 23:44:02 +0530

    Added the people model

---
+++ models/People.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export const PeopleSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please enter a valid email address",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const People = mongoose.model("People", PeopleSchema);
